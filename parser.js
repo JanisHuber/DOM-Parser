a2cf8bfe53e1abb70da14ffd0905d43e400e272e
JanisHuber <janis.huber@itme.ch>
2024-11-13 09:32:53 +0100

    Finished

--- parser.js
+++ parser.js
@@ -1,9 +1,13 @@
 var result = "";
 
 function parseNode(startNode) {
-    //todo
-    //für jeden Kindknoten ab "startNode" eine Tabellenzeile (<tr><td>....</tr>) 
-    //mit den gemäss Aufgabenstellung gewünschten Infos erstellen
-
+    var node = startNode;
+    while (node) {
+        result += "<tr><td>" + node.nodeType + "</td><td>" + node.nodeName + "</td><td>" + node.nodeValue + "</td><td>" + node.textContent + "</td></tr>";
+        for (var i = 0; i < node.childNodes.length; i++) {
+            parseNode(node.childNodes[i]);
+        }
+        node = node.nextSibling;
+    }
     return "<table><tr><th>nodeType</th><th>nodeName</th><th>nodeValue</th><th>textContent</th></tr>" + result + "</table>";
 }
